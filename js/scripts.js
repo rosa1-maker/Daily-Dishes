08d490046b6d46df4e45b14e3fdb32dbbe07555e
rosa1-maker <teresawanjiku2000@gmail.com>
2019-12-19 08:22:29 +0300

    added a total price for the order made

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,16 @@ function validateForm() {
       alert(name + ", Thank you for your comment.");
     }
     event.preventDefault();
+  }var sizePrices = [1000, 650, 350];
+  var deliveryPrices = [0, 200];
+  function TotalPrice(price, quantity, delivery, toppings, crust) {
+    this.price = price;
+    this.quantity = quantity;
+    this.delivery = delivery;
   }
+  TotalPrice.prototype.finalTotal = function() {
+    return (
+      (this.price + this.delivery ) * this.quantity
+    );
+  };
+  
