afaf204dfc4a48c380f014ca94b6994d55bb2791
rosa1-maker <teresawanjiku2000@gmail.com>
2019-12-19 09:39:04 +0300

    added variable of prices

--- js/scripts.js
+++ js/scripts.js
@@ -20,4 +20,91 @@ function validateForm() {
       (this.price + this.delivery ) * this.quantity
     );
   };
+  TotalPrice.prototype.finalTotal = function() {
+    return (
+      (this.price + this.delivery ) * this.quantity
+    );
+  };
+  $(document).ready(function() {
+    $("form#myorder").submit(function(event) {
+      event.preventDefault();
+      var pizzaType = $('#type').val();
+      var pizzaSize = parseInt($('#size').val());
+      var pizzaToppings=parseInt($('#topings').val());
+      var priceCrust =parseInt($('#crust').val());
+      var pizzaTop = $('#topings').val();
+      var pizzaQuant = parseInt($('#quantity').val());
+      var pizzaPick = parseInt($('#delivery').val());
+      var price = sizePrices[pizzaSize - 1];
+      var DeliveryCost = deliveryPrices[pizzaPick - 1];
+      var ToppingsCost = priceToppings[pizzaToppings-1];
+      var crustCost = crustPrices[priceCrust-1]
+      var topNames = toppingsName[pizzaTop-1]
+      var crustName = crustNames[priceCrust-1] 
+  
+      newOrder = new Results(
+        pizzaType,
+        pizzaSize,
+        crustName,
+        topNames,
+        crustName
+      );
+      newTotal = new TotalPrice(
+        price,
+        pizzaQuant,
+        DeliveryCost,
+        ToppingsCost,
+        crustCost
+      );
+      function Results(type, size, crust, toppings) {
+          this.type = type;
+          this.size = size;
+          this.crust = crust;
+          this.toppings = toppings;
+        }
+        Results.prototype.order = function() {
+          return (
+            "You have made an order of " +
+            this.type +
+            " pizza  with " +
+            this.toppings +
+            " as toppings and " +
+            this.crust +
+            " for crust ."
+          );
+        };
+      if (pizzaPick === 1) {
+        alert(newOrder.order());
+        alert("Your bill is: " + newTotal.finalTotal());
+        alert("Thank you for shopping with us! ");
+      } else if (pizzaPick === 2) {
+        var location = prompt(
+          " Where would your like your Pizza to be Delivered? "
+        );
+        var locations = ["gpo", "ngong plaza", "trm", "cbd"];
+        if (
+          location !== locations[0] &&
+          location !== locations[1] &&
+          location !== locations[2] &&
+          location !== locations[3]
+        ) {
+          alert("Choose a location listed below");
+        } else {
+          prompt(
+            "Please enter your phone number to facilitate communication and faster delivery."
+          );
+          alert(
+            "Your order has been received and it will be delivered to " +
+              location +
+              " An additional 200/= will be charged for delivery."
+          );
+          alert(newOrder.order());
+          alert("Your bill is: " + newTotal.finalTotal());
+          alert(
+            "Thank you for shopping with us! Your Order will be arriving soon."
+          );
+        }
+      }
+    });
+  });
   
